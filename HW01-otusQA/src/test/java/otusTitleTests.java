78d6320b682bb5373c160208761321e3b5cfd0f9
MuTalKing <sg.dayver@gmail.com>
2021-01-12 21:04:04 +0300

    Сделал отдельный модуль для первого домашнего задания Создал тест на проверку урл и тест на проверку title страницы Сделал логиравание в консоль и файл с помощью log4j2 Параметризировал конфиги с помощью owner

---
+++ HW01-otusQA/src/test/java/otusTitleTests.java
@@ -0,0 +1,61 @@
+import config.ServerConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class otusTitleTests {
+
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(otusTitleTests.class);
+    private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
+    }
+
+    @Test
+    public void checkUrl(){
+        driver.get(cfg.url());
+        logger.info("Открыта страница Отуса");
+        String actualUrl = driver.getCurrentUrl();
+        logger.info("Проверяем соответствие title страницы");
+        if(!actualUrl.equals(cfg.url())){
+            logger.error("URL страницы не соответствует");
+        }
+        else {
+            logger.info("URL страницы соответствует");
+        }
+    }
+
+    @Test
+    public void checkTitle(){
+        driver.get(cfg.url());
+        logger.info("Открыта страница Отуса");
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
+        logger.info("Проверяем соответствие title страницы");
+        if(!actualTitle.equals(expectedTitle)){
+            logger.error("Title страницы не соответствует");
+        }
+        else {
+            logger.info("Title страницы соответствует");
+        }
+    }
+
+    @After
+    public void setDown(){
+        if (driver != null){
+            driver.quit();
+        }
+        logger.info("Драйвер опущен");
+    }
+}
