68f8304fb163947136db303ab8584d5c7069f899
MuTalKing <sg.dayver@gmail.com>
2021-01-19 22:59:40 +0300

    Реализовал WebDriverFactory класс Метод createDriver() принимает обязательный параметр webDriverName и необязтельный параметр options, а возвращает соответствующий имени вебдрайвер с заданными (если были) options регистр значения параметра -Dbrowser не влияет на результат для хранения имен драйверов используется Enum

---
+++ HW02-WebDriverFactory/src/test/java/otusTitleAndURLTests.java
@@ -0,0 +1,57 @@
+import config.Browsers;
+import config.ServerConfig;
+import config.WebDriverFactory;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class otusTitleAndURLTests {
+
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(otusTitleAndURLTests.class);
+    private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+
+    @Before
+    public void setUp() {
+        String browserName = System.getProperty("browser");
+        driver = WebDriverFactory.createDriver(browserName);
+        logger.info("Драйвер поднят");
+    }
+
+    @Test
+    public void checkUrl(){
+        driver.get(cfg.url());
+        logger.info("Открыта страница Отуса");
+        String actualUrl = driver.getCurrentUrl();
+        logger.info("Проверяем соответствие URL страницы");
+        AssertionError assertionError = null;
+
+        try{
+            Assert.assertEquals(actualUrl, cfg.url());
+        }
+        catch(AssertionError aEr){
+            assertionError = aEr;
+        }
+        finally{
+            if(assertionError == null){
+                logger.info("URL страницы соответствует");
+            }else{
+                logger.error("URL страницы не соответствует");
+                throw assertionError;
+            }
+        }
+    }
+
+    @After
+    public void setDown(){
+        if (driver != null){
+            driver.quit();
+        }
+        logger.info("Драйвер опущен");
+    }
+}
