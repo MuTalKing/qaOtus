97b844ba2487bdff4dcb7b1f02fd4228b802ac09
MuTalKing <sg.dayver@gmail.com>
2021-02-23 19:33:54 +0300

    Сделал работу над ошибками

--- HW04-PageObject/src/main/java/otus/LoginPage.java
+++ HW04-PageObject/src/main/java/otus/LoginPage.java
@@ -1,6 +1,7 @@
 package otus;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -9,34 +10,38 @@ import static otus.BaseSteps.log;
 
 public class LoginPage {
 
-    By authorizationButton = By.xpath("//button[contains(@class,'js-open-modal')]");
-    By loginField = By.xpath("//input[contains(@class, 'js-email-input')]");
-    By passwordField = By.xpath("//input[contains(@class, 'js-psw-input')]");
-    By loginButton = By.xpath("//button[contains(@class, 'new-button_md')]");
+    private By authorizationButton = By.xpath("//button[contains(@class,'js-open-modal')]");
+    private By loginField = By.xpath("//input[contains(@class, 'js-email-input')]");
+    private By passwordField = By.xpath("//input[contains(@class, 'js-psw-input')]");
+    private By loginButton = By.xpath("//button[contains(@class, 'new-button_md')]");
+
+    public WebElement getElement(By locator){
+        return new WebDriverWait(driverChrome, 10).until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
 
     public LoginPage clickAuthorizationButton(){
-        new WebDriverWait(driverChrome, 10).until(ExpectedConditions.presenceOfElementLocated(authorizationButton)).click();
+        getElement(authorizationButton).click();
         log.info("Нажимаем на кнопку 'Войти'");
 
         return this;
     }
 
     public LoginPage filLoginField(String email){
-        new WebDriverWait(driverChrome, 10).until(ExpectedConditions.presenceOfElementLocated(loginField)).sendKeys(email);
+        getElement(loginField).sendKeys(email);
         log.info("Вводим email");
 
         return this;
     }
 
     public LoginPage filPasswordField(String password){
-        new WebDriverWait(driverChrome, 10).until(ExpectedConditions.presenceOfElementLocated(passwordField)).sendKeys(password);
+        getElement(passwordField).sendKeys(password);
         log.info("Вводим пароль");
 
         return this;
     }
 
     public LoginPage clickLoginButton(){
-        new WebDriverWait(driverChrome, 10).until(ExpectedConditions.presenceOfElementLocated(loginButton)).click();
+        getElement(loginButton).click();
         log.info("Входим под своей учетной записью");
 
         return this;
--- HW04-PageObject/src/main/java/otus/OtusPage.java
+++ HW04-PageObject/src/main/java/otus/OtusPage.java
@@ -11,40 +11,41 @@ import static otus.BaseSteps.driverChrome;
 
 public class OtusPage {
 
-    By userField = By.xpath("//div[@class='header2-menu__item-wrapper header2-menu__item-wrapper__username']");
-    By myProfile = By.xpath("//b[@class='header2-menu__dropdown-text_name'][text()='Сергей Гоголев']");
-    By contactField = By.xpath("//div[@class='input input_full lk-cv-block__input input_straight-bottom-right input_straight-top-right input_no-border-right lk-cv-block__input_fake lk-cv-block__input_select-fake js-custom-select-presentation']");
-    By facebookButton = By.xpath("//button[@title='Facebook']");
-    By facebookField = By.xpath("//input[@name='contact-0-value']");
-    By vkButton = By.xpath("//div[@data-num='1']//button[@title='VK']");
-    By vkField = By.xpath("//input[@name='contact-1-value']");
-    By secondContactField = By.xpath("//div[@data-num='1']//div[@class='input input_full lk-cv-block__input input_straight-bottom-right input_straight-top-right input_no-border-right lk-cv-block__input_fake lk-cv-block__input_select-fake js-custom-select-presentation']");
-    By addButton = By.xpath("//button[@type='button'][text()='Добавить']");
-    By saveButton = By.xpath("//button[@title='Сохранить и продолжить']");
+    private By userField = By.xpath("//div[@class='header2-menu__item-wrapper header2-menu__item-wrapper__username']");
+    private By myProfile = By.xpath("//b[@class='header2-menu__dropdown-text_name'][text()='Сергей Гоголев']");
+    private By contactField = By.xpath("//div[@class='input input_full lk-cv-block__input input_straight-bottom-right input_straight-top-right input_no-border-right lk-cv-block__input_fake lk-cv-block__input_select-fake js-custom-select-presentation']");
+    private By facebookButton = By.xpath("//button[@title='Facebook']");
+    private By facebookField = By.xpath("//input[@name='contact-0-value']");
+    private By vkButton = By.xpath("//div[@data-num='1']//button[@title='VK']");
+    private By vkField = By.xpath("//input[@name='contact-1-value']");
+    private By secondContactField = By.xpath("//div[@data-num='1']//div[@class='input input_full lk-cv-block__input input_straight-bottom-right input_straight-top-right input_no-border-right lk-cv-block__input_fake lk-cv-block__input_select-fake js-custom-select-presentation']");
+    private By addButton = By.xpath("//button[@type='button'][text()='Добавить']");
+    private By saveButton = By.xpath("//button[@title='Сохранить и продолжить']");
 
+    public WebElement getElement(By locator){
+        return new WebDriverWait(driverChrome, 10).until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
 
 
     public OtusPage openPersonalSettings(){
-        WebElement userFieldElement = new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(userField));
-        action.moveToElement(userFieldElement).build().perform();
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(myProfile)).click();
+        action.moveToElement(getElement(userField)).build().perform();
+        getElement(myProfile).click();
         log.info("Открываем 'Мой профиль'");
 
         return this;
     }
 
     public OtusPage setInformationAndSave(){
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(contactField)).click();
-        WebElement telegramButtonElement = new WebDriverWait(driverChrome,10).until(ExpectedConditions.elementToBeClickable(facebookButton));
-        telegramButtonElement.click();
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(facebookField)).sendKeys("gogolev");
+        getElement(contactField).click();
+        getElement(facebookButton).click();
+        getElement(facebookField).sendKeys("gogolev");
         log.info("Указываем контакт Фейсбука");
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(addButton)).click();
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(secondContactField)).click();
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(vkButton)).click();
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(vkField)).sendKeys("gogolev");
+        getElement(addButton).click();
+        getElement(secondContactField).click();
+        getElement(vkButton).click();
+        getElement(vkField).sendKeys("gogolev");
         log.info("Указываем контакт ВК");
-        new WebDriverWait(driverChrome,10).until(ExpectedConditions.presenceOfElementLocated(saveButton)).click();
+        getElement(saveButton).click();
         log.info("Сохраняем данные");
 
         return this;
